3cb446200446459acd6b0cb120c625e90c2c6694
Tiagorosa78 <202200167@estudantes.ips.pt>
2023-11-17 10:20:51 +0000

    Nivel 1

--- src/main/java/pt/pa/Main.java
+++ src/main/java/pt/pa/Main.java
@@ -3,6 +3,9 @@ package pt.pa;
 import pt.pa.graph.Edge;
 import pt.pa.graph.GraphLinked;
 import pt.pa.graph.Vertex;
+import pt.pa.model.Person;
+import pt.pa.model.UniversityNetwork;
+
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +22,38 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        UniversityNetwork universityNetwork = new UniversityNetwork();
 
+        Person student1 = new Person(135, "Rodrigo", Person.PersonRole.STUDENT);
+        Person student2 = new Person(215, "Cátia", Person.PersonRole.STUDENT);
+        Person student3 = new Person(18, "Alberto", Person.PersonRole.STUDENT);
+        Person student4 = new Person(231, "Rita", Person.PersonRole.STUDENT);
+        Person student5 = new Person(2, "Ana", Person.PersonRole.STUDENT);
+        Person student6 = new Person(131, "Pedro", Person.PersonRole.STUDENT);
+        Person student7 = new Person(235, "Joao", Person.PersonRole.STUDENT);
+
+        Person docente1 = new Person(35, "Pedro", Person.PersonRole.TEACHER);
+        Person docente2 = new Person(31, "Ana", Person.PersonRole.TEACHER);
+
+        universityNetwork.addPerson(student1);
+        universityNetwork.addPerson(student2);
+        universityNetwork.addPerson(student3);
+        universityNetwork.addPerson(student4);
+        universityNetwork.addPerson(student5);
+        universityNetwork.addPerson(student6);
+        universityNetwork.addPerson(student7);
+        universityNetwork.addPerson(docente1);
+        universityNetwork.addPerson(docente2);
+
+        universityNetwork.addGroupRelationship("Colegas de Grupo, Algebra 1 ", student1.getId(), student2.getId());
+        universityNetwork.addClassRelationship("Algebra Geral ", docente2.getId(), student1.getId());
+        universityNetwork.addClassRelationship("Algebra Geral", docente2.getId(), student2.getId());
+        universityNetwork.addClassRelationship("Analise Matematica", docente2.getId(), student3.getId());
+        universityNetwork.addClassRelationship("Analise Matematica", docente2.getId(), student6.getId());
+        universityNetwork.addGroupRelationship("Colegas De Tuna", student3.getId(), student4.getId());
+        universityNetwork.addClassRelationship("PA", docente1.getId(), student6.getId());
+        universityNetwork.addGroupRelationship("Colega de Grupo PA.1", student6.getId(), student7.getId());
+        universityNetwork.addClassRelationship("PA", docente1.getId(), student7.getId());
+        universityNetwork.addClassRelationship("PA", docente1.getId(), student3.getId());
     }
 }
--- src/main/java/pt/pa/model/UniversityNetwork.java
+++ src/main/java/pt/pa/model/UniversityNetwork.java
@@ -27,6 +27,45 @@ public class UniversityNetwork {
         return null;
     }
 
+    public void addPerson(Person p) throws NetworkException {
+        if (findPerson(p.getId()) != null) {
+            throw new NetworkException("Person with ID " + p.getId() + " already exists in the network.");
+        }
+        network.insertVertex(p);
+    }
+
+    public void addGroupRelationship(String description, int idStudent1, int idStudent2) throws NetworkException {
+        Vertex<Person> student1 = findPerson(idStudent1);
+        Vertex<Person> student2 = findPerson(idStudent2);
+
+        if (student1 == null || student2 == null) {
+            throw new NetworkException("One or both students do not exist in the network.");
+        }
+
+        if (!student1.element().isRole(Person.PersonRole.STUDENT) || !student2.element().isRole(Person.PersonRole.STUDENT)) {
+            throw new NetworkException("One or both IDs do not refer to students.");
+        }
+
+        Relationship groupRelationship = new Relationship(description, Relationship.RelRole.GROUP);
+        network.insertEdge(student1, student2, groupRelationship);
+    }
+
+    public void addClassRelationship(String description, int idTeacher, int idStudent) throws NetworkException {
+        Vertex<Person> teacher = findPerson(idTeacher);
+        Vertex<Person> student = findPerson(idStudent);
+
+        if (teacher == null || student == null) {
+            throw new NetworkException("One or both persons do not exist in the network.");
+        }
+
+        if (teacher.element().getRole() != Person.PersonRole.TEACHER || student.element().getRole() != Person.PersonRole.STUDENT) {
+            throw new NetworkException("One or both IDs do not refer to the correct type.");
+        }
+
+        Relationship classRelationship = new Relationship(description, Relationship.RelRole.CLASS);
+        network.insertEdge(teacher, student, classRelationship);
+    }
+
 
 
 }
